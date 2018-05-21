b67eade6d77b61ffe4817e6740d47feabee9179f
Stella Lu <stella-lu@outlook.com>
2018-05-21 23:10:12 +1200

    Returns labels and frequencies of words

--- main.py
+++ main.py
@@ -1,38 +1,69 @@
 import re
-import requests
+#import requests
 import string
 import webbrowser
-from bs4 import BeautifulSoup
+import wikipedia
+#from bs4 import BeautifulSoup
 
-def get_text(str_to_remove, page):
-	soup = BeautifulSoup(page.text, 'html.parser') # Create a BeautifulSoup object
-	p_text = soup.find_all('p')
-	str_text = make_one_string(p_text)
-	text = ' '.join(str_text) # One string including html tags
-	
+str_to_remove = ['[clarification needed]', '\xa0']
+
+def modify_text(text):
+	#soup = BeautifulSoup(page.text, 'html.parser')
+	#p_text = soup.find_all('p') + soup.find_all('td') + soup.find_all('span')
+	#str_text = make_one_string(p_text)
+	#text = ' '.join(str_text) # String including unwanted strings & chars
+
 	# Remove html tags and other irrelevant strings
 	text = remove_html_tags(text)
-	for s in str_to_remove:
-		if s in text:
-			text = text.replace(s, '')
+	text = remove_misc_str(text, str_to_remove)
 	translator = str.maketrans('', '', string.punctuation)
-	return text.translate(translator)
+	text= text.translate(translator)
 
-def make_one_string(p_text):
-	for i in range(len(p_text)):
-		p_text[i] = str(p_text[i])
-	return p_text
+	# Turn long string into list of words
+	text = re.sub("[^\w]", " ",  text).split()
+	
+	return text
+
+def make_one_string(text):
+	""" Takes a list and turns each element into a string """
+	for i in range(len(text)):
+		text[i] = str(text[i])
+	return text
 
 def remove_html_tags(text):
-    """Remove html tags from a string"""
+    """ Remove html tags from a string """
     clean = re.compile('<.*?>')
     return re.sub(clean, '', text)
 
-if __name__ == "__main__":
-	#str_to_remove = ['[clarification needed]', '\xa0', '(', ')', '.', ',', ';', '"']
-	str_to_remove = ['[clarification needed]', '\xa0']
-	main_page = 'https://en.wikipedia.org/wiki/Tuareg_rebellion_(1990%E2%80%931995)'
-	page = requests.get(main_page)
+def remove_misc_str(text, str_to_remove):
+	""" Remove strings in list str_to_remove from text """
+	for s in str_to_remove:
+		if s in text:
+			text = text.replace(s, '')
+	return text
 
-	text = get_text(str_to_remove, page)
-	print(text)
+def count(lst):
+	""" Takes one string and returns a list of labels and a list of word frequencies"""
+	labels, freq = [], []
+
+	for word in lst:
+		if word not in labels:
+			labels.append(word)
+			freq.append(1)
+		else:
+			index = labels.index(word)
+			freq[index] = freq[index] + 1
+
+	return labels, freq
+
+if __name__ == "__main__":
+	main_page = 'https://en.wikipedia.org/wiki/Mila_Kunis_filmography'
+	content = wikipedia.page('Beaurières').content
+	#page = requests.get(main_page)
+
+	lst_of_text = modify_text(content)
+	labels, freq = count(lst_of_text)
+
+	print(labels, freq)
+
+
